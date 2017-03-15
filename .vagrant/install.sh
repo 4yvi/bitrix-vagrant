74bb7f2729c8079a73ab3189b382927ead72c186
Alexey Trofimov <gadruit@gmail.com>
2017-03-15 12:43:33 +0300

    Исправил проблему с установкой расширений

--- .vagrant/install.sh
+++ .vagrant/install.sh
@@ -24,53 +24,53 @@ yum install php70u-fpm-nginx php70u-cli php70u-mysqlnd -y
 
 # Устанавливаем расширения для php
 yum install gcc make -y
-yum install php-pear php-devel -y
+yum install php70u-pear php-devel -y
 
 # Список расширений для установки (Можно добавить свои)
-yum install php-bz2 -y
-yum install php-calendar -y
-yum install php-cgi-fcgi -y
-yum install php-core -y
-yum install php-ctype -y
-yum install php-date -y
-yum install php-exif -y
-yum install php-fileinfo -y
-yum install php-filter -y
-yum install php-ftp -y
-yum install php-gettext -y
-yum install php-gmp -y
-yum install php-hash -y
-yum install php-iconv -y
-yum install php-libxml -y
-yum install php-openssl -y
-yum install php-pcntl -y
-yum install php-pcre -y
-yum install php-phar -y
-yum install php-readline -y
-yum install php-reflection -y
-yum install php-session -y
-yum install php-shmop -y
-yum install php-simplexml -y
-yum install php-sockets -y
-yum install php-spl -y
-yum install php-sqlite3 -y
-yum install php-standard -y
-yum install php-tokenizer -y
-yum install php-xml -y
-yum install php-zlib -y
-yum install php-curl -y
-yum install php-dom -y
-yum install php-gd -y
-yum install php-json -y
-yum install php-mbstring -y
-yum install php-mcrypt -y
-yum install php-mysqli -y
-yum install php-mysqlnd -y
-yum install php-opcache -y
-yum install php-pdo -y
-yum install php-pdo_mysql -y
-yum install php-soap -y
-yum install php-xmlrpc -y
+yum install php70u-bz2 -y
+yum install php70u-calendar -y
+yum install php70u-cgi-fcgi -y
+yum install php70u-core -y
+yum install php70u-ctype -y
+yum install php70u-date -y
+yum install php70u-exif -y
+yum install php70u-fileinfo -y
+yum install php70u-filter -y
+yum install php70u-ftp -y
+yum install php70u-gettext -y
+yum install php70u-gmp -y
+yum install php70u-hash -y
+yum install php70u-iconv -y
+yum install php70u-libxml -y
+yum install php70u-openssl -y
+yum install php70u-pcntl -y
+yum install php70u-pcre -y
+yum install php70u-phar -y
+yum install php70u-readline -y
+yum install php70u-reflection -y
+yum install php70u-session -y
+yum install php70u-shmop -y
+yum install php70u-simplexml -y
+yum install php70u-sockets -y
+yum install php70u-spl -y
+yum install php70u-sqlite3 -y
+yum install php70u-standard -y
+yum install php70u-tokenizer -y
+yum install php70u-xml -y
+yum install php70u-zlib -y
+yum install php70u-curl -y
+yum install php70u-dom -y
+yum install php70u-gd -y
+yum install php70u-json -y
+yum install php70u-mbstring -y
+yum install php70u-mcrypt -y
+yum install php70u-mysqli -y
+yum install php70u-mysqlnd -y
+yum install php70u-opcache -y
+yum install php70u-pdo -y
+yum install php70u-pdo_mysql -y
+yum install php70u-soap -y
+yum install php70u-xmlrpc -y
 
 # Рестарт
 systemctl restart php-fpm
